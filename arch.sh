37d0cd2adc5c78a98dece85480e47bfdbf9cd9d7
RangHo Lee <zu0107@gmail.com>
2023-12-23 03:49:50 +0000

    fix: use correct path for environment variable cache

--- arch.sh
+++ arch.sh
@@ -9,9 +9,9 @@ esac
 
 rootfs=$(cd "$(dirname "$0")"/.. && pwd)
 oldpwd=$(pwd)
-export | sudo tee "$rootfs/tmp/.env.sh"
+export | sudo tee "$rootfs/bounce/env.sh" >/dev/null
 
 exec sudo "$rootfs/bin/arch-chroot" "$rootfs" \
 	/bin/su "$user" \
-		/bin/sh -lc ". /tmp/.env.sh; cd '$oldpwd' 2>/dev/null; exec \"\$@\"" -- \
+		/bin/sh -lc ". /bounce/env.sh; cd '$oldpwd' 2>/dev/null; exec \"\$@\"" -- \
 			/bin/sh -eo pipefail "$@"
