69b14adf37cf34dc4f2ba0d703a5741543bd670e
RangHo Lee <zu0107@gmail.com>
2023-12-23 03:54:54 +0000

    fix: hardcode `GITHUB_WORKSPACE`

--- arch.sh
+++ arch.sh
@@ -8,10 +8,9 @@ case "$1" in
 esac
 
 rootfs=$(cd "$(dirname "$0")"/.. && pwd)
-oldpwd=$(pwd)
 export | sudo tee "$rootfs/bounce/env.sh" >/dev/null
 
 exec sudo "$rootfs/bin/arch-chroot" "$rootfs" \
 	/bin/su "$user" \
-		/bin/sh -lc ". /bounce/env.sh; cd '$oldpwd' 2>/dev/null; exec \"\$@\"" -- \
+		/bin/sh -lc ". /bounce/env.sh; cd '$GITHUB_WORKSPACE' 2>/dev/null; exec \"\$@\"" -- \
 			/bin/sh -eo pipefail "$@"
